234df2f47d815b8f9f2951d80900fcafa210c66b
zinnomeister <zinnomeister@gmail.com>
2022-10-01 15:10:38 +0100

    Add project files.

---
+++ HW-ListOfUsers/Program.cs
@@ -0,0 +1,128 @@
+﻿using SOHW_listOfUsers;
+using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Channels;
+//using System.Threading.Tasks;
+//using System.IO;
+//using System.Globalization;
+
+namespace SOHW_listOfUsers
+{
+    class Program
+    {
+        static List<User> allUsers = new List<User>();
+
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\zinno\OneDrive\Разработка\Storage\ListOfUsers.txt";
+
+        Action:
+            Console.WriteLine("If you want to load list of users from file - press 'F5'");
+            Console.WriteLine("If you want to add user - press 'Enter' or any key");
+            Console.WriteLine("If you want to remove user - press 'Delete'");
+            Console.WriteLine("If you want to print list of users - press 'F1'");
+            Console.WriteLine("If you want to save list of users to file - press 'F10'");
+            Console.WriteLine("If you want to clear list of users - press 'F8'");
+            Console.WriteLine("If you want to exit - press 'Escape'");
+
+            ConsoleKey key = Console.ReadKey().Key;
+            if (key == ConsoleKey.Enter)
+                AddUser();
+
+            else if (key == ConsoleKey.Delete)
+                DeleteUser();
+
+            else if (key == ConsoleKey.F1)
+
+                foreach (var item in allUsers)
+                    PrintList(item);
+
+            else if (key == ConsoleKey.Escape)
+                Environment.Exit(0);
+
+            else if (key == ConsoleKey.F10)
+                SaveToFile(filePath);
+
+            else if (key == ConsoleKey.F5)
+                ExtractFromFile(filePath);
+
+            else if (key == ConsoleKey.F8)
+                allUsers.Clear();
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine();
+            Console.WriteLine($"Quantity of users {allUsers.Count}");
+            Console.ResetColor();
+
+            goto Action;
+
+            Console.ReadLine();
+        }
+        static void AddUser()
+        {
+            User user = new User();
+            user.Id = Guid.NewGuid();
+            Console.WriteLine("\nInput name:\t");
+            user.Name = Console.ReadLine();
+            Console.WriteLine("\nInput phone number:\t");
+            user.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("\nInput password:\t");
+            user.Password = Console.ReadLine();
+            allUsers.Add(user);
+        }
+
+        static void DeleteUser()
+        {           
+            Console.WriteLine("\nInput the name of user:\t");
+            var userToDelete = Console.ReadLine();
+            allUsers.RemoveAll(x => x.Name == userToDelete);
+            Console.WriteLine($"\nUser {userToDelete} was deleted successfully");
+
+            Console.WriteLine();
+        }
+
+        static void PrintList(User user)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t------------------------------------------------------------------");
+            Console.ResetColor();
+            Console.WriteLine($"{allUsers.IndexOf(user) + 1}.\t Name: {user.Name}"
+                + $"\t|Phone: {user.PhoneNumber}"
+                + $"\t|Password:  {user.Password}"
+                + $"\t|ID: {user.Id}");
+        }
+
+        static void SaveToFile(string filepath)
+        {
+            string[] allUsersString = new string[allUsers.Count];
+
+            for (int i = 0; i < allUsersString.Length; i++)
+            {
+                User user = allUsers[i];
+                allUsersString[i] = user.GetFullName();
+            }
+
+            File.WriteAllLines(filepath, allUsersString);
+        }
+
+        static void ExtractFromFile(string filepath)
+        {
+            List<string> allUsersFromFile = File.ReadAllLines(filepath).ToList();
+            foreach (string usersString in allUsersFromFile)
+            {
+                User userFromFile = new User();
+                string[] users = usersString.Split('|');
+                userFromFile.Id = new Guid(users[0]);
+                userFromFile.Name = users[1];
+                userFromFile.PhoneNumber = users[2];
+                userFromFile.Password = users[3];
+                allUsers.Add(userFromFile);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HW-ListOfUsers/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SOHW_listOfUsers
+{
+    public class User
+    {
+        public Guid Id;
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+        public string GetFullName()
+        {
+            return $"{Id}|{Name}|{PhoneNumber}|{Password}";
+        }
+    }
+}
